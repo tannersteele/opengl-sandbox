ad5a32179f8ff83d42ed16f8d3a65176ed9d6e1f
Tanner Steele <tannerjsteele@gmail.com>
2024-04-03 21:36:30 -0500

    Scalar vs vector benchmarking tests
    With optimization enabled, I'm the clear loser here:
    Vector add timing: 22300ns
    Scalar add timing 15800ns
    
    Since the compiler doesn't try to optimize away my vector add code, it runs slower comparatively to the assembly it generates for a simple scalar add.
    
    But for the sake of debug (no optimization flags), AVX2 crushes it by ~3x:
    Vector add timing: 43000ns
    Scalar add timing 115300ns

--- opengl-sandbox/avx2vectortest.cpp
+++ opengl-sandbox/avx2vectortest.cpp
@@ -1,12 +1,15 @@
 #include <immintrin.h>
 #include <iostream>
 #include <assert.h>
+#include <chrono>
 
 constexpr int FLOAT_OPS = 256 / (sizeof(float) * 8); // 256-bit wide register / sizeof float in bits
 const int TEST_ARR_SIZE = 65536;
 
 void addVectorTest(const float*, const float*, float*);
-void fillVectorArray(float*, float*);
+void addScalarTest(const float*, const float*, float*);
+
+void fillArrays(float*, float*);
 
 int main()
 {
@@ -14,13 +17,26 @@ int main()
 	float b[TEST_ARR_SIZE];
 	float result[TEST_ARR_SIZE];
 
-	fillVectorArray(a, b);
-	addVectorTest(a, b, result);
+	// Vector Add ------------------------
+	fillArrays(a, b);
 
+	auto startTime = std::chrono::high_resolution_clock::now();
+	addVectorTest(a, b, result);
+	auto endTime = std::chrono::high_resolution_clock::now();
+	std::cout << "Vector add timing: " << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count() << "ns" << std::endl;
+
+	// Scalar Add ------------------------
+	fillArrays(a, b); // Reset to produce similar results
+	
+	startTime = std::chrono::high_resolution_clock::now();
+	addScalarTest(a, b, result);
+	endTime = std::chrono::high_resolution_clock::now();
+
+	std::cout << "Scalar add timing " << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count() << "ns" << std::endl;
 	return 0;
 }
 
-void fillVectorArray(float* arrA, float* arrB)
+void fillArrays(float* arrA, float* arrB)
 {
 	for (int i = 0; i < TEST_ARR_SIZE; ++i)
 	{
@@ -29,13 +45,21 @@ void fillVectorArray(float* arrA, float* arrB)
 	}
 }
 
+void addScalarTest(const float* a, const float* b, float* result)
+{
+	for (int i = 0; i < TEST_ARR_SIZE; i++) // scalar operation, add 1 pair at a time
+	{
+		result[i] = a[i] + b[i];
+	}
+}
+
 void addVectorTest(const float* a, const float* b, float* result)
 {
 	for (int i = 0; i < TEST_ARR_SIZE; i += FLOAT_OPS)
 	{
-		__m256 avx_a = _mm256_loadu_ps(&a[i]); // loadu will load 8 32-bit floats at a time
+		__m256 avx_a = _mm256_loadu_ps(&a[i]); // loadu will load 8*32-bit floats at a time
 		__m256 avx_b = _mm256_loadu_ps(&b[i]);
 
-		_mm256_storeu_ps(&result[i], _mm256_add_ps(avx_a, avx_b));
+		_mm256_storeu_ps(&result[i], _mm256_add_ps(avx_a, avx_b)); //SIMD add operation (8*32-bit floats simultaneously) + store in results arr
 	}
 }
